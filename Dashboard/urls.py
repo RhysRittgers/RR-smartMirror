666d6db15d9e1870af75b3396583917eb9691296
RhysRittgers <rhysrittgers@gmail.com>
2025-08-11 20:47:41 -0400

    Namespace Dashboard urls

--- Dashboard/urls.py
+++ Dashboard/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import mobile_dashboard
 
+app_name = "Dashboard"
+
 urlpatterns = [
     path('', mobile_dashboard, name='mobile_dashboard')
 ]
---
+++ spotify/urls.py
@@ -0,0 +1,12 @@
+# src/spotify/urls.py
+from django.urls import path
+from . import views
+
+app_name = "spotify"  # optional but recommended
+
+urlpatterns = [
+    path("login/", views.spotify_login, name="login"),
+    path("callback/", views.spotify_callback, name="callback"),
+    path("current/", views.current_track, name="current"),
+    path("control/<str:action>/", views.control, name="control"),
+]
