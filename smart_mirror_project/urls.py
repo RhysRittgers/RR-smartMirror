d168a8f62d2898efb96fccd17f419c6813275ae8
RhysRittgers <rhysrittgers@gmail.com>
2025-10-08 17:17:39 -0400

    fixed hoem view import

--- smart_mirror_project/urls.py
+++ smart_mirror_project/urls.py
@@ -3,10 +3,11 @@ from django.urls import path, include
 from .diag import diag
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('','home', name='home'),
+    path("", home, name="home"),
     path('auth/', include('Authentication.urls')),
     path('Calendar/', include('Calendar.urls')),
     path('Message/', include('Message.urls')),
