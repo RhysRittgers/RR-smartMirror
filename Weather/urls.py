2cf1f71adf5bbe3d88efe3c004e759cf858f8529
RhysRittgers <rhysrittgers@gmail.com>
2026-09-01 12:25:45 -0400

    went back to before weather forecast update frontend fixed url

--- Weather/urls.py
+++ Weather/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import weather_preference
+from .views import weather_preference, weekly_forecast
 
 urlpatterns = [
 	path('user_weather/', weather_preference, name='user_weather'),
+	path('forecast/', weekly_forecast, name='forecast'),
 ]
